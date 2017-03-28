e81def2fa0176c509469f84f1431d6c801df21d8
hailongxiong <hailongxiong@126.com>
2017-03-28 03:22:31 -0400

    singleton mode test

---
+++ singleton.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class singleton
+{
+    private:
+	static singleton* A;
+	singleton();
+	singleton(const singleton&);
+	singleton& operator=(const singleton&);
+
+    public:
+	static singleton* getInstance();
+};
+
+singleton::singleton(){}
+singleton::singleton(const singleton&){}
+singleton& singleton::operator=(const singleton&){}
+singleton* singleton::A = new singleton();
+singleton* singleton::getInstance() {
+		return A;
+}
+
+
+int main()
+{
+	cout<<"test singleton.\n";
+
+	singleton* singleton1 = singleton::getInstance();
+        singleton* singleton2 = singleton::getInstance();
+
+	if (singleton1 == singleton2)
+		cout<<"the object is the same.\n";
+	else
+		cout<<"the object is not the same.\n";
+
+	return 1;
+}
